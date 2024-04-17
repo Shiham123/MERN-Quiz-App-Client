eee9e3597ba09a08338773b301678c19eea6f029
Shiham123 <s36651786@gmail.com>
2024-04-17 11:14:23 +0600

    question show with static not setup with backend

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react"
+import data from "../../database/data.js"
 
 const Questions = () => {
 	const [checked, setChecked] = useState(false)
@@ -7,28 +8,39 @@ const Questions = () => {
 		setChecked(true)
 	}
 
+	const question = data[0]
+
 	return (
 		<div>
 			<h2 className="font-Poppins text-2xl font-semibold">Simple Question</h2>
-			<ul>
-				<li className="flex justify-end items-center gap-4 flex-row-reverse">
-					<div>
-						<input
-							type="radio"
-							id="q1-option"
-							value={false}
-							name="options"
-							onChange={handleChooseAnswer}
-							className="appearance-none "
-						/>
-						<label htmlFor="q1-option">Option</label>
-					</div>
-					<div className="w-6 h-6 rounded-full border-4 border-gray-300 relative">
-						{checked && (
-							<div className="absolute top-[2px] left-[2px] bg-gray-400 w-3 h-3 rounded-full"></div>
-						)}
-					</div>
-				</li>
+			<ul key={question.id}>
+				{question.options?.map((item, index) => {
+					return (
+						<li key={index} className="flex justify-end items-center gap-4 flex-row-reverse">
+							<div>
+								<input
+									type="radio"
+									id={`q${index}-option`}
+									value={false}
+									name="options"
+									onChange={handleChooseAnswer}
+									className="appearance-none"
+								/>
+								<label
+									htmlFor={`q${index}-option`}
+									className="font-Poppins text-xl font-semibold tracking-widest"
+								>
+									{item}
+								</label>
+							</div>
+							<div className="w-6 h-6 rounded-full border-4 border-gray-300 relative">
+								{checked && (
+									<div className="absolute top-[2px] left-[2px] bg-gray-400 w-3 h-3 rounded-full"></div>
+								)}
+							</div>
+						</li>
+					)
+				})}
 			</ul>
 		</div>
 	)
