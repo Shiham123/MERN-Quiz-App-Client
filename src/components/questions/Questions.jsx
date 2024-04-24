a79a8ff3b5d9dcffdc7b74aa0df2da2980424c1e
Shiham123 <s36651786@gmail.com>
2024-04-24 10:02:21 +0600

    get data from data json file successfully created

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -1,46 +1,53 @@
 import {useState} from "react"
-import data from "../../database/data.js"
+import useFetchQuestion from "../../hooks/useFetchQuestion.jsx"
 
 const Questions = () => {
+	const [getData, setGetData] = useFetchQuestion()
 	const [checked, setChecked] = useState(false)
 
 	const handleChooseAnswer = () => {
 		setChecked(true)
 	}
 
-	const question = data[0]
+	if (!getData.apiData || getData.apiData.length === 0) {
+		return <div>Loading...</div>
+	}
+
+	const question = getData.apiData[0]
+
+	if (!question.options || question.options.length === 0) {
+		return <div>No options available for this question.</div>
+	}
 
 	return (
 		<div>
 			<h2 className="font-Poppins text-2xl font-semibold">Simple Question</h2>
 			<ul key={question.id}>
-				{question.options?.map((item, index) => {
-					return (
-						<li key={index} className="flex justify-end items-center gap-4 flex-row-reverse">
-							<div>
-								<input
-									type="radio"
-									id={`q${index}-option`}
-									value={false}
-									name="options"
-									onChange={handleChooseAnswer}
-									className="appearance-none"
-								/>
-								<label
-									htmlFor={`q${index}-option`}
-									className="font-Poppins text-xl font-semibold tracking-widest"
-								>
-									{item}
-								</label>
-							</div>
-							<div className="w-6 h-6 rounded-full border-4 border-gray-300 relative">
-								{checked && (
-									<div className="absolute top-[2px] left-[2px] bg-gray-400 w-3 h-3 rounded-full"></div>
-								)}
-							</div>
-						</li>
-					)
-				})}
+				{question.options.map((item, index) => (
+					<li key={index} className="flex justify-end items-center gap-4 flex-row-reverse">
+						<div>
+							<input
+								type="radio"
+								id={`q${index}-option`}
+								value={false}
+								name="options"
+								onChange={handleChooseAnswer}
+								className="appearance-none"
+							/>
+							<label
+								htmlFor={`q${index}-option`}
+								className="font-Poppins text-xl font-semibold tracking-widest"
+							>
+								{item}
+							</label>
+						</div>
+						<div className="w-6 h-6 rounded-full border-4 border-gray-300 relative">
+							{checked && (
+								<div className="absolute top-[2px] left-[2px] bg-gray-400 w-3 h-3 rounded-full"></div>
+							)}
+						</div>
+					</li>
+				))}
 			</ul>
 		</div>
 	)
--- src/hooks/useFetchQuestion.jsx
+++ src/hooks/useFetchQuestion.jsx
@@ -8,22 +8,22 @@ const useFetchQuestion = () => {
 	const [getData, setGetData] = useState({isLoading: false, apiData: [], serverError: null})
 
 	useEffect(() => {
-		setGetData((prev) => ({...prev, isLoading: true}))(async () => {
+		const fetchData = async () => {
+			setGetData((prev) => ({...prev, isLoading: true}))
 			try {
 				let question = await data
-
 				if (question.length > 0) {
-					setGetData((prev) => ({...prev, apiData: question}))
-					setGetData((prev) => ({...prev, isLoading: false}))
+					setGetData((prev) => ({...prev, apiData: question, isLoading: false}))
 					dispatch(startExamAction(question))
 				} else {
 					throw new Error("no question available")
 				}
 			} catch (error) {
-				setGetData((prev) => ({...prev, isLoading: false}))
-				setGetData((prev) => ({...prev, serverError: error}))
+				setGetData((prev) => ({...prev, isLoading: false, serverError: error}))
 			}
-		})()
+		}
+
+		fetchData()
 	}, [dispatch])
 
 	return [getData, setGetData]
