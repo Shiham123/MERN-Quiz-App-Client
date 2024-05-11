ab502f8952c3bb7b0992dc7423edf29c53de36ab
Shiham123 <s36651786@gmail.com>
2024-05-11 18:38:03 +0600

    try to use rtk query in my quiz client sidte

--- src/app/store.js
+++ src/app/store.js
@@ -2,8 +2,10 @@ import {configureStore, combineReducers} from "@reduxjs/toolkit"
 import themeSlice from "./Theme/themeSlice"
 import questionSlice from "./services/questionSlice"
 import resultSlice from "./services/resultSlice"
+import {addUserApi} from "./api/userApi"
 
 const rootReducer = combineReducers({
+	[addUserApi.reducerPath]: addUserApi.reducer,
 	theme: themeSlice,
 	question: questionSlice,
 	result: resultSlice,
@@ -12,6 +14,7 @@ const rootReducer = combineReducers({
 const store = configureStore({
 	reducer: rootReducer,
 	// middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([addUserApi.middleware]),
 })
 
 export default store
