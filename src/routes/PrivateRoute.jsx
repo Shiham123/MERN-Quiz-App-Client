8b394fdafbf7ae0a1d8d0ee38bc28622f0d46bde
Shiham123 <s36651786@gmail.com>
2024-05-12 17:01:05 +0600

    navigate user to home and prevent user to submit double time quiz done

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,10 +3,18 @@ import {Navigate} from "react-router-dom"
 import PropTypes from "prop-types"
 
 const PrivateRoute = ({children}) => {
-	// const {mutations} = useSelector((state) => state.user)
+	const {mutations} = useSelector((state) => state.user)
+	let navigateHome = false
+
+	Object.values(mutations).forEach((mutation) => {
+		if (mutation.status === "rejected") {
+			navigateHome = true
+		}
+	})
+
 	const {userId} = useSelector((state) => state.result)
 
-	return userId ? children : <Navigate to={"/"} replace={true} />
+	return !navigateHome && userId ? children : <Navigate to={"/"} replace={true} />
 }
 
 export default PrivateRoute
