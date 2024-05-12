8a5feac6abef57720eb0dd01e05c6985b021f223
Shiham123 <s36651786@gmail.com>
2024-05-12 10:21:03 +0600

    ALHAMDULLILAH rtk query error solved successfully connected

--- src/app/store.js
+++ src/app/store.js
@@ -1,19 +1,20 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit"
-import logger from "redux-logger"
 
 import themeSlice from "./Theme/themeSlice"
 import questionSlice from "./services/questionSlice"
 import resultSlice from "./services/resultSlice"
+import {userApi} from "./api/userApi"
 
 const rootReducer = combineReducers({
 	theme: themeSlice,
 	question: questionSlice,
 	result: resultSlice,
+	[userApi.reducerPath]: userApi.reducer,
 })
 
 const store = configureStore({
 	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware),
 })
 
 export default store
