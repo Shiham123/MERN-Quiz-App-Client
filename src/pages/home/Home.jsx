18f8f4dea33f87ede84e69da67beb733badd319f
Shiham123 <s36651786@gmail.com>
2024-05-22 22:11:53 +0600

    framar motion installed and successfully initially use in the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,20 @@
+import {motion} from "framer-motion"
+
+// components
 import Illustration from "../../components/HomeComponents/Illustration"
 import MainPage from "../../components/HomeComponents/MainPage"
 
 const Home = () => {
 	return (
-		<div
+		<motion.div
+			initial={{opacity: 0, scale: 0}}
+			animate={{opacity: 1, scale: 1}}
+			transition={{type: "spring", stiffness: 80, damping: 20}}
 			className={`2xl:flex xl:flex lg:flex md:flex md:flex-col sm:flex sm:flex-col xs:flex xs:flex-col flex py-[9rem] px-8 scrollbar-hide justify-center items-center gap-32`}
 		>
 			<Illustration />
 			<MainPage />
-		</div>
+		</motion.div>
 	)
 }
 
