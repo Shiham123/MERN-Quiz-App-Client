73ddb6953a57c1b27065ed108b9a0f1ccbbd74e2
Shiham123 <s36651786@gmail.com>
2024-05-26 11:06:20 +0600

    right left section

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -9,10 +9,13 @@ import Questions from "../questions/Questions"
 import {moveNextAction, moveToPrevAction} from "../../app/services/questionSlice"
 import {pushResultAction} from "../../app/services/resultSlice"
 import {isQuizCheck} from "../../app/Theme/themeSlice"
+import useFetchQuestion from "../../hooks/useFetchQuestion"
+import Loader from "../../shared/Loader"
 
 const Quiz = () => {
+	const [{apiData, isLoading, serverError}] = useFetchQuestion()
 	const {trace, queue, answer} = useSelector((state) => state.question)
-	const {result} = useSelector((state) => state.result)
+	const {result, userId} = useSelector((state) => state.result)
 
 	const dispatch = useDispatch()
 
@@ -29,8 +32,16 @@ const Quiz = () => {
 		return <Navigate to={`/result`} replace={true} />
 	}
 
+	if (isLoading || serverError) return <Loader />
+
 	return (
-		<div className="my-10 flex justify-center items-center flex-col w-1/2">
+		<div className="my-10 flex justify-center items-center flex-col w-1/2 relative">
+			{userId && (
+				<div className="absolute -top-[10rem] -right-[3rem] bg-gradient-to-tr from-[#c33741] via-[#d73e48] to-[#ed4550] text-2xl px-8 py-4 rounded-lg text-white font-Poppins font-semibold">
+					Question {trace + 1}/ {apiData?.question?.length}
+				</div>
+			)}
+
 			<Questions />
 
 			<div className="flex justify-between items-center gap-8 my-12">
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,6 +4,7 @@ import ErrorPage from "../pages/Error/ErrorPage"
 import Home from "../pages/home/Home"
 import Result from "../components/quiz/Result"
 import QuizPage from "../components/quiz/QuizPage"
+import PrivateRoute from "./PrivateRoute"
 
 const router = createBrowserRouter([
 	{
@@ -14,7 +15,11 @@ const router = createBrowserRouter([
 			{index: true, element: <Home />},
 			{
 				path: "/quiz",
-				element: <QuizPage />,
+				element: (
+					<PrivateRoute>
+						<QuizPage />
+					</PrivateRoute>
+				),
 			},
 			{path: "/result", element: <Result />},
 		],
