08e2ef41e1150749a83f1a574809cad55af424d4
Shiham123 <s36651786@gmail.com>
2024-05-29 18:14:20 +0600

    result page redesigning

--- src/components/quiz/Result.jsx
+++ src/components/quiz/Result.jsx
@@ -1,28 +1,22 @@
 import {useDispatch, useSelector} from "react-redux"
 import {Link} from "react-router-dom"
+import {useEffect} from "react"
 
 // shared component
 import QuizHeading from "../../shared/QuizHeading"
 import QuizBtn from "../../shared/QuizBtn"
-import PerDiv from "../../shared/PerDiv"
-
-// component
-import ResultTable from "./ResultTable"
 
 // redux action
 import {resetResultAction} from "../../app/services/resultSlice"
 import {resetAllAction} from "../../app/services/questionSlice"
-import {useEffect} from "react"
 import {getFinalResult} from "../../app/Theme/themeSlice"
 
+// scss file
+import style from "../../scss/Result.module.scss"
+
 const Result = () => {
-	const {result, correctAnswers, finalResult} = useSelector((state) => ({
-		queue: state.question.queue,
-		result: state.result.result,
-		correctAnswers: state.question.correctAnswers,
-		userId: state.result.userId,
-		finalResult: state.theme.finalResult,
-	}))
+	const {result} = useSelector((state) => state.result)
+	const {correctAnswers} = useSelector((state) => state.question)
 
 	const dispatch = useDispatch()
 
@@ -47,26 +41,45 @@ const Result = () => {
 	}, [dispatch, correctAnswers, result])
 
 	return (
-		<div className="max-w-xl mx-auto my-10 flex justify-center items-center flex-col">
-			<QuizHeading headerText="Quiz Result" />
+		<div className={`flex justify-center items-center flex-col ${style.resultBg} py-[11rem]`}>
+			<div className={`${style.resultSecondBg} w-2/3 bg-white py-10`}>
+				<QuizHeading headerText="Quiz Result" />
 
-			{/* Per div for user history */}
-			<div className="border-[2px] border-black/50 w-full p-4 m-4 rounded-lg">
-				<PerDiv actions="User Name" userName="Shiham Ibne Yousuf" />
-				<PerDiv actions="Total Quiz Points : " userName="20" />
-				<PerDiv actions="Total Questions : " userName="05" />
-				<PerDiv actions="Total Attempts : " userName="03" />
-				<PerDiv actions="Total Earn Points : " userName="30" />
-				<PerDiv actions="Quiz Result" userName="Passed" />
+				<section className="flex flex-col justify-center items-center my-10">
+					{/* div one */}
+					<div className="flex justify-between items-center bg-[#f5f5f5] rounded-full w-2/3 my-2">
+						<div className="bg-[#ededed] flex justify-between rounded-full w-2/3">
+							<h1 className="font-Poppins text-4xl font-bold py-6 px-20 bg-[#e2e2e2] rounded-full tracking-wide text-center">
+								Your score:
+							</h1>
+							<h1 className="font-Poppins text-4xl font-bold py-6 px-8 rounded-full">60% </h1>
+						</div>
+						<h1 className="font-Poppins text-4xl font-bold py-6 px-8 rounded-full">60 Points</h1>
+					</div>
+
+					{/* div two */}
+					<div className="flex justify-between items-center bg-[#f5f5f5] rounded-full w-2/3 my-2">
+						<div className="bg-[#ededed] flex justify-between rounded-full w-2/3">
+							<h1 className="font-Poppins text-4xl font-bold py-6 px-14 bg-[#e2e2e2] rounded-full">
+								Passing score:
+							</h1>
+							<h1 className="font-Poppins text-4xl font-bold py-6 px-8 rounded-full">80% </h1>
+						</div>
+						<h1 className="font-Poppins text-4xl font-bold py-6 px-8 rounded-full">80 Points</h1>
+					</div>
+
+					<div>
+						<h2>Result</h2>
+						<p>You did not pass the test</p>
+						<p>Better luck next time</p>
+					</div>
+				</section>
+
+				{/* Restart quiz actions */}
+				<Link to={"/"}>
+					<QuizBtn startQuizBtnText="Restart" onClick={handleRestart} />
+				</Link>
 			</div>
-
-			{/* Restart quiz actions */}
-			<Link to={"/"}>
-				<QuizBtn startQuizBtnText="Restart" onClick={handleRestart} />
-			</Link>
-
-			{/* Result table  */}
-			<ResultTable userResult={finalResult} />
 		</div>
 	)
 }
